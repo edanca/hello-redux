492391ce43cdbd41c6b8b64e811c49e03f42531d
Eduardo Cárdenas <edu.card@gmail.com>
2019-05-06 01:21:52 -0300

    Simple redux app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import reducer from "./reducers/Reducer";
+import { createStore } from 'redux';
+
+const initialState = { tech: "React " };
+const store = createStore(reducer, initialState);
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
   render() {
     return <HelloWorld tech={this.state.tech} />;
   }
